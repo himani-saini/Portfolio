7c4bc665dffb64927bcabc2c10c1a4fb87fdf53c
Himani Saini <sainihimani2003@gmail.com>
2024-06-13 23:44:59 +0530

    third commit

--- src/sections/contactsec.vue
+++ src/sections/contactsec.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="container my-5">
     <div class="row">
@@ -7,73 +6,69 @@
         <h2 class="mt-3">Let's discuss</h2>
         <h2>something cool together</h2>
         <div class="img-content">
-        <img :src="require('@/assets/contact.gif')" class="img-fluid" alt="Contact">
-      </div>
-      </div>
-      <div class="col-12 col-md-6 d-flex flex-column ">
-        <label>Email Address </label>
-        <input v-model="address" type="text" placeholder="Enter email address" class="form-control mb-3 bg-light">
-          <label class="w-100">Message</label>
-          <textarea v-model="message" name="user_message" id="user_message" 
-          cols="20" rows="10" class="bg-light py-2 px-4 w-100 mb-3 col-xl-6 form-control">
-          </textarea>
-          <button @click="sendData" class="btn col-md-6 w-10 mx-auto" style="background-color: #DCCFED;">Send</button>
+          <img :src="require('@/assets/contact.gif')" class="img-fluid" alt="Contact">
         </div>
-        
+      </div>
+      <div class="col-12 col-md-6 d-flex flex-column">
+        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="contact" />
+          <label>Email Address</label>
+          <input v-model="address" type="text" name="email" placeholder="Enter email address" class="form-control mb-3 bg-light" required>
+          <label class="w-100">Message</label>
+          <textarea v-model="message" name="message" id="user_message" cols="20" rows="10" class="bg-light py-2 px-4 w-100 mb-3 col-xl-6 form-control" required></textarea>
+          <p class="hidden">
+            <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+          </p>
+          <button type="submit" class="btn col-md-6 w-10 mx-auto" style="background-color: #DCCFED;">Send</button>
+        </form>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'contactsec',
-    data(){
-      return{
-      address:'',
-      message:'',
-      }
-    },
-
-    methods: {
-      sendData(){
-
-      }
-    },
+  name: 'contact-sec',
+  data() {
+    return {
+      address: '',
+      message: '',
+    };
   }
+};
 </script>
 
-  <style scoped>
-  h1,h2{
-    font-family: 'Josefin Sans';
-    text-align: justify;
-    color: #331C52
-  }
-  .img-content{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width:100%
-  }
-  img{
-    height:auto;
-    width:50%;
-    
-  }
-  
-  input,label,button{
-    font-family: 'Josefin Sans';
-  }
+<style scoped>
+h1, h2 {
+  font-family: 'Josefin Sans';
+  text-align: justify;
+  color: #331C52;
+}
 
-  .container{
-    margin-top: 8rem !important;
-  }
+.img-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
 
-  @media(max-width:768px){
-   .row{
+img {
+  height: auto;
+  width: 50%;
+}
+
+input, label, button {
+  font-family: 'Josefin Sans';
+}
+
+.container {
+  margin-top: 8rem !important;
+}
+
+@media (max-width: 768px) {
+  .row {
     flex-direction: row-reverse;
     flex-wrap: wrap;
-   }
   }
-  </style>
+}
+</style>
--- src/sections/mainsec.vue
+++ src/sections/mainsec.vue
@@ -1,29 +1,29 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="container my-5">
-    <div class="row flex-wrap-reverse">
+    <div class="row flex-md-row-reverse">
       <!-- Text column -->
-      <div class="col-md-6 d-flex flex-column justify-content-center">
+      <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center align-items-md-start text-center text-md-left">
         <h2>Hello👋,</h2>
         <h1>I'm a Web Developer</h1>
-        <p class="mt-4 ">
+        <p class="mt-4">
           I am a front-end developer who constantly seeks out innovative solutions to everyday problems.
-          As a beginner, I'm passionate about mastering the fundamentals of web design and development, eagerly soaking up knowledge and honing my skills with each project. 
+          As a beginner, I'm passionate about mastering the fundamentals of web design and development, eagerly soaking up knowledge and honing my skills with each project.
         </p>
-        <div class="d-flex justify-content-center mt-2 gap-3">
+        <div class="d-flex justify-content-center justify-content-md-start mt-2 gap-3">
           <a :href="instagramProfile" aria-label="Instagram" target="_self">
-            <font-awesome-icon :icon="['fab', 'instagram']" size="lg" style="color:black"/>
+            <font-awesome-icon :icon="['fab', 'instagram']" size="lg" style="color:black" />
           </a>
           <a :href="githubProfile" aria-label="Github" target="_self">
             <font-awesome-icon :icon="['fab', 'github']" size="lg" style="color:black" />
           </a>
-          <a v-bind:href="pinterestProfile" aria-label="Pinterest" target="_self">
+          <a :href="pinterestProfile" aria-label="Pinterest" target="_self">
             <font-awesome-icon :icon="['fab', 'pinterest']" size="lg" style="color:black;" />
           </a>
         </div>
       </div>
-       <!-- Image column -->
-       <div class="col-md-6 d-flex justify-content-center">
+      <!-- Image column -->
+      <div class="col-12 col-md-6 d-flex justify-content-center">
         <img :src="require('@/assets/profile3.jpg')" class="img-fluid" alt="Profile Picture">
       </div>
     </div>
@@ -34,14 +34,13 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
-  data(){
-    return{
-      instagramProfile:'https://www.instagram.com/camaraderie_9/',
-      githubProfile:'https://github.com/himani-saini',
-      pinterestProfile:'https://in.pinterest.com/awwwards/'
+  data() {
+    return {
+      instagramProfile: 'https://www.instagram.com/camaraderie_9/',
+      githubProfile: 'https://github.com/himani-saini',
+      pinterestProfile: 'https://in.pinterest.com/awwwards/'
     }
   },
-
   components: {
     FontAwesomeIcon,
   }
@@ -49,18 +48,16 @@ export default {
 </script>
 
 <style scoped>
-h2,h1,
-p {
+h2, h1, p {
   font-family: 'Josefin Sans';
   text-align: left;
 }
-p{
-  font-size:18px;
+p {
+  font-size: 18px;
 }
 .container {
   margin-top: 5rem !important;
 }
-
 img {
   max-width: 100%;
   height: auto;
